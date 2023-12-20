b60d50bc34becdb27802cc6a3c85b50528eb3041
Ronaldo <Ronaldo-172010@hotmail.com>
2023-12-19 23:56:55 -0300

    Alterado Ui do player, e botões

--- app/src/main/java/com/example/soundplayer/constants/Constants.kt
+++ app/src/main/java/com/example/soundplayer/constants/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
      const val KEY_EXTRA_MUSIC ="SONG"
      const val NOTIFICATION_CHANNEL_ID ='1'
      const val NOTIFICATION_ID =100
+     const val DATABASE_NAME ="playlist_database"
 
 }
